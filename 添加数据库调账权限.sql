02fb4e3390782474d759b9a29bd66dcfb401dcba
ab1324ab <435821607@qq.com>
2018-11-09 15:12:07 +0800

    Create 添加数据库调账权限.sql

---
+++ 添加数据库调账权限.sql
@@ -0,0 +1,6 @@
+--------添加数据库调账权限-----------------------------------------
+
+
+insert into sysdb.T_SYS_ROLE_MENUITEM (ROLE_ID, MENUITEM_ID, MENU_ID, NEED_TWO_AUTH) values ('adminrole', 'upAccount', '17', '0');
+
+
